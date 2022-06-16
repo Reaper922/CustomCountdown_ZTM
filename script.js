3484e976b3a5e6e911c103954669913d51ec524b
Dennis Ammen <dennis.ammen@outlook.com>
2022-06-16 23:12:49 +0200

    JS Dates

---
+++ script.js
@@ -0,0 +1,6 @@
+const inputContainer = document.getElementById('input-container');
+const countdownForm = document.getElementById('coundown-form');
+const dateEl = document.getElementById('date-picker');
+
+const today = new Date().toISOString().split('T')[0];
+dateEl.setAttribute('min', today);
