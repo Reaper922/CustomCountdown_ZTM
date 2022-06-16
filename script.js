7e389065cf038a81e279b6abf0be0c492f95b044
Dennis Ammen <dennis.ammen@outlook.com>
2022-06-16 23:21:46 +0200

    Submitting Form Values

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const inputContainer = document.getElementById('input-container');
-const countdownForm = document.getElementById('coundown-form');
+const countdownForm = document.getElementById('countdown-form');
 const dateEl = document.getElementById('date-picker');
+
+let countdownTitle = '';
+let countdownDate = '';
 
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+function updateCountdown(event) {
+    event.preventDefault();
+    countdownTitle = event.srcElement[0].value;
+    countdownDate = event.srcElement[1].value;
+    console.log(countdownTitle, countdownDate);
+}
+
+countdownForm.addEventListener('submit', updateCountdown);
